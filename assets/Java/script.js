c27b38764d2f553380f24f2b52090d0083b9fbdd
Jason Barbanel <jbarbanel@gmail.com>
2021-02-24 14:56:10 -0500

    Adding variables to java file

--- assets/Java/script.js
+++ assets/Java/script.js
@@ -1,14 +1,37 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Input Variables
 
-  passwordText.value = password;
+var upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var password = [uppercase, lowercase, specialcharacters, numbers]
 
-}
+// Decleration Variables
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var confirmLength= ““ ;
+var confirmUpper;
+var confirmLower;
+var confirmSpecial;
+var confirmNumbers;
+
+
+
+
+
+
+
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
